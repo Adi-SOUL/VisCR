5b317177a4489f79694ef1f59fba83ce80b70892
Adijnnuuy <96059201+Adi-SOUL@users.noreply.github.com>
2024-07-08 17:15:48 +0800

    update

--- camera.cpp
+++ camera.cpp
@@ -58,16 +58,15 @@ void Camera::setCameraPos(const QVector3D &value) {
 void Camera::handle(QEvent *e) {
     if(e->type()==QEvent::MouseMove) {
         if(!press) {
-            widget->setCursor(Qt::BlankCursor);             //隐藏鼠标光标
+            widget->setCursor(Qt::BlankCursor);             
             widget->setMouseTracking(true);
             QMouseEvent *event=static_cast<QMouseEvent*>(e);
             float xoffset = event->x() - widget->rect().center().x();
-            float yoffset = widget->rect().center().y() - event->y(); // 注意这里是相反的，因为y坐标是从底部往顶部依次增大的
-            xoffset *= sensitivity;
+            float yoffset = widget->rect().center().y() - event->y();
             yoffset *= sensitivity;
             yaw   += xoffset;
             pitch += yoffset;
-            if(pitch > 1.55)         //将俯视角限制到[-89°,89°]，89°约等于1.55
+            if(pitch > 1.55)         
                 pitch =  1.55;
             if(pitch < -1.55)
                 pitch = -1.55;
@@ -76,31 +75,31 @@ void Camera::handle(QEvent *e) {
             cameraDirection.setZ(sin(yaw)*cos(pitch));
             view.setToIdentity();
             view.lookAt(cameraPos,cameraPos+cameraDirection,cameraUp);
-            QCursor::setPos(widget->geometry().center());       //将鼠标移动窗口中央
+            QCursor::setPos(widget->geometry().center());       
         } else {
-            widget->setCursor(Qt::ArrowCursor);   //恢复鼠标光标
+            widget->setCursor(Qt::ArrowCursor);   
             widget->setMouseTracking(false);
         }
     } else if(e->type()==QEvent::Timer) {
         float cameraSpeed = moveSpeed * deltaTime;
-        if (keys.contains(Qt::Key_W))                           //前
+        if (keys.contains(Qt::Key_W))                           
             cameraPos+=cameraSpeed * cameraDirection;
-        if (keys.contains(Qt::Key_S))                           //后
+        if (keys.contains(Qt::Key_S))                           
             cameraPos -= cameraSpeed * cameraDirection;
-        if (keys.contains(Qt::Key_A))                           //左
+        if (keys.contains(Qt::Key_A))                           
             cameraPos-=QVector3D::crossProduct(cameraDirection,cameraUp)*cameraSpeed;
-        if (keys.contains(Qt::Key_D))                           //右
+        if (keys.contains(Qt::Key_D))                           
             cameraPos+=QVector3D::crossProduct(cameraDirection,cameraUp)*cameraSpeed;
-        if (keys.contains(Qt::Key_Space))                       //上浮
+        if (keys.contains(Qt::Key_Space))                       
             cameraPos.setY(cameraPos.y()+cameraSpeed);
-        if (keys.contains(Qt::Key_Shift)) {                      //下沉
+        if (keys.contains(Qt::Key_Shift)) {                      
             float y = cameraPos.y()-cameraSpeed;
             cameraPos.setY(y);
         }
-        if (keys.contains(Qt::Key_U)) {                      //下沉
+        if (keys.contains(Qt::Key_U)) {                      
             moveSpeed += 0.01;
         }
-        if (keys.contains(Qt::Key_J)) {                      //下沉
+        if (keys.contains(Qt::Key_J)) {                      
             moveSpeed -= 0.01;
         }
         if (keys.contains(Qt::Key_Alt)) {
@@ -109,10 +108,9 @@ void Camera::handle(QEvent *e) {
         view.setToIdentity();
         view.lookAt(cameraPos,cameraPos+cameraDirection,cameraUp);
     } else if(e->type()==QEvent::KeyPress) {
-        //isAutoRepeat用于判断此按键的来源是否是长按
         QKeyEvent *event=static_cast<QKeyEvent*>(e);
-        keys.insert(event->key());                              //添加按键
-        if(!event->isAutoRepeat()&&timeId==0) {                 //如果定时器未启动，则启动定时器
+        keys.insert(event->key());                              
+        if(!event->isAutoRepeat()&&timeId==0) {                 
             timeId=widget->startTimer(1);
         }
     } else if(e->type()==QEvent::KeyRelease) {
@@ -121,29 +119,29 @@ void Camera::handle(QEvent *e) {
             press = false;
         }
         keys.remove(event->key());
-        if(!event->isAutoRepeat()&&timeId!=0&&keys.empty()) {   //当没有按键按下且定时器正在运行，才关闭定时器
+        if(!event->isAutoRepeat()&&timeId!=0&&keys.empty()) {   
             widget->killTimer(timeId);
-            timeId=0;                                          //重置定时器id
+            timeId=0;                                          
         }
     } else if(e->type()==QEvent::UpdateRequest) {
         float time=QTime::currentTime().msecsSinceStartOfDay()/1000.0;
-        deltaTime = time - lastFrame;                           //在此处更新时间差
+        deltaTime = time - lastFrame;                           
         lastFrame = time;
     } else if(e->type()==QEvent::FocusIn) {
-        widget->setCursor(Qt::BlankCursor);             //隐藏鼠标光标
-        QCursor::setPos(widget->geometry().center());   //将鼠标移动窗口中央
-        widget->setMouseTracking(true);                 //开启鼠标追踪
+        widget->setCursor(Qt::BlankCursor);             
+        QCursor::setPos(widget->geometry().center());   
+        widget->setMouseTracking(true);                 
         press = false;
     } else if(e->type()==QEvent::FocusOut) {
-        widget->setCursor(Qt::ArrowCursor);   //恢复鼠标光标
-        widget->setMouseTracking(false);      //关闭鼠标追踪
+        widget->setCursor(Qt::ArrowCursor);   
+        widget->setMouseTracking(false);      
     }
 
 }
 
 void Camera::init() {
     view.lookAt(cameraPos,cameraPos+cameraDirection,cameraUp);
-    widget->activateWindow();                 //激活窗口
+    widget->activateWindow();                
     widget->setFocus();
 
 }
